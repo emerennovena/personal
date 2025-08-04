c69bcea770f77ea6b22670debb4c13089fd9571e
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-08-04 16:48:37 +0100

    updaate

--- src/data/GradProjects.js
+++ src/data/GradProjects.js
@@ -1,21 +1,21 @@
-
+import languatale1 from '../assets/images/languatale.png'
 
 export default [
     {
         id: 1,
-        title: "Down",
-        icon: null,
-        description1: "Capstone Project Course: Project in Software System Design. ",
-        description2: "Built a mobile application, a service for users to explore, join and create local events",
+        title: "LanguaTale",
+        icon: languatale1,
+        description1: "Master Dissertation: Story-based Language Learning Web App. ",
+        description2: "Built a web application to let users learn a language through stories",
         githubPath: null,
         demoPath: null,
-        year: "2021",
-        techUsed: "Adobe XD, React Native"
+        year: "2025",
+        techUsed: "Django, Figma"
     },
     {
       id: 2,
-      title: "Sleep Tracker",
-      description1: "Mobile application that tracks overnight sleep and sleepiness levels during the day. Data is analyzed and displayed as insights.",
+      title: "CinemaTown",
+      description1: "Mobile application that allow users to explore, join and create local events.",
       icon: null,
       githubPath: "https://www.google.com/url?q=https%3A%2F%2Fgithub.com%2Fcheryllg%2FSleep-Tracker-Ionic&sa=D",
       demoPath: null,
