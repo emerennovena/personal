59e4bd5e4f603202a452ef3e3fcf7418caa25e5e
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-08-04 16:28:15 +0100

    updaate

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/src/pages/404.js")),
+  "component---src-pages-aboutme-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/src/pages/aboutme.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/src/pages/contact.js")),
+  "component---src-pages-experience-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/src/pages/experience.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/src/pages/index.js")),
+  "component---src-pages-projects-js": preferDefault(require("/Users/vincentiasutardji/IdeaProjects/personal_website/src/pages/projects.js"))
+}
+
