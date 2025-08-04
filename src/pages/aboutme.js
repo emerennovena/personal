cfededcdba7183914dbe0dfc7d4d827d68f32ab8
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-08-04 15:09:48 +0100

    updaate

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -11,11 +11,12 @@ const AboutMe = () => (
         
         <p style={{ color: '#395696'}}>
             <h1>Why Business Technology?</h1>
-            ata is everywhere. It is all around us, a rich minefield of information that
-            can easily be transformed into useful insights and solutions to real world problems.<br/>
+            I’m studying Software Development but I’m most interested in combining business and technology.
+            I enjoy using tools like CRMs and automation platforms to solve real business problems efficiently.<br/>
 
-            <br/> As a child, I’ve always loved patterns and found puzzle solving to be an exhilaratingly
-            challenging pastime. And that’s what data is to me, a complicated puzzle that I can't wait to solve!</p>
+            <br/>As a child, I was always into everything: arts, languages, music. But technology allows me to learn
+            new things every day, keeping me stimulated and challenged.
+        </p>
     </RecRec>
       <h3 style={{ color: '#395696'}}>Interests</h3>
         <RecDes><ul><li style={{ color: '#395696'}}>Classical Literature</li>
