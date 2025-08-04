92e47bdd924efff0d6c6a57f172b9ef2170102c8
Emerentia Novena <3058284n@student.gla.ac.uk>
2025-08-04 15:16:21 +0100

    updaate

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import { RecWrapper, RecBox, RecDes, RecRec } from '../styles/recStyle.js'
 import Layout from '../components/layout'
-import me from '../assets/images/me.jpg'
+import newme from '../assets/images/newme.jpg'
 
 const AboutMe = () => (
   <Layout>
   <RecWrapper>
     <RecRec>
-        <img src={me} height="500px" />
+        <img src={newme} height="500px" />
         
         <p style={{ color: '#395696'}}>
             <h1>Why Business Technology?</h1>
             I’m studying Software Development but I’m most interested in combining business and technology.
             I enjoy using tools like CRMs and automation platforms to solve real business problems efficiently.<br/>
 
-            <br/>As a child, I was always into everything: arts, languages, music. But technology allows me to learn
+            <br/>As a child, I was always into everything: arts, languages, music. Technology allows me to learn
             new things every day, keeping me stimulated and challenged.
         </p>
     </RecRec>
       <h3 style={{ color: '#395696'}}>Interests</h3>
         <RecDes><ul><li style={{ color: '#395696'}}>Classical Literature</li>
+            <li style={{ color: '#395696'}}>Language Learning</li>
             <li style={{ color: '#395696'}}>Stardew Valley</li>
             </ul></RecDes>
         <hr/>
